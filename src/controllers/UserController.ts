7c47578b69dacaa41d139c169602df0c5d0d96be
TheMaestro0 <abdovc012@gmail.com>
2021-08-23 21:20:13 +0200

    /users/userId - GET

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,21 @@
+import { Response, Request } from '@tinyhttp/app'
 import * as web from 'express-decorators'
+import { User } from '../structures'
 
 @web.basePath('/users')
-export default class UserController {}
+export default class UserController {
+    @web.get('/:userId')
+    async getUser(req: Request, res: Response): Promise<void> {
+        const { userId } = req.params
+
+        const user = await db.em.findOne(User, {
+            _id: userId === '@me' ? req.user._id : userId
+        }, ['username', 'avatar', 'badges', '_id'])
+
+        if (!user) {
+            return void res.sendStatus(404)
+        }
+
+        res.json(user)
+    }
+}
