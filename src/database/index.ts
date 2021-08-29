99162a1ca0b1ab96d299aaa778eaed368a0b0d8f
TheMaestro0 <abdovc012@gmail.com>
2021-08-29 17:20:12 +0200

    format database file.

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,13 +3,13 @@ import { Message, User, Server, Member, DMChannel, TextChannel, Group } from '..
 
 class Database {
 	private db!: MikroORM
-	
+
 	get<T extends AnyEntity<T>, U extends EntityRepository<T> = EntityRepository<T>>(entityName: EntityName<T>): GetRepository<T, U> {
 		return this.db.em.getRepository(entityName)
 	}
 
-    async connect(clientUrl: string): Promise<this> {
-		
+	async connect(clientUrl: string): Promise<this> {
+
 		this.db = await MikroORM.init({
 			clientUrl,
 			type: 'mongo',
@@ -17,7 +17,7 @@ class Database {
 			dbName: 'b9s8hx7mvxwjetc',
 			debug: false
 		})
-		
+
 
 		return this
 	}
