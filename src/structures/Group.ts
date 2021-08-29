2ed85f4425c0a658ea4ff69c3f7ba003e9b1c757
TheMaestro0 <abdovc012@gmail.com>
2021-08-29 15:52:56 +0200

    Add more properties to groups

--- src/structures/Group.ts
+++ src/structures/Group.ts
@@ -3,6 +3,7 @@ import { Channel } from '.'
 import { ChannelTypes } from './Channel'
 
 export interface CreateGroupOptions extends Omit<Partial<Group>, 'type'> {
+    name: string
     ownerId: string
     recipients: string[]
 }
@@ -11,6 +12,15 @@ export interface CreateGroupOptions extends Omit<Partial<Group>, 'type'> {
 export class Group extends Channel {
     @Property()
     readonly type = ChannelTypes.GROUP
+
+    @Property()
+    name!: string
+
+    @Property({ nullable: true })
+    description?: string
+
+    @Property({ nullable: true })
+    icon?: string
 
     @Property()
     ownerId!: string
