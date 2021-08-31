a87cbd191e477da43219be58b7a7038fc4189989
TheMaestro0 <abdovc012@gmail.com>
2021-08-31 02:46:16 +0200

    Remove GroupController

--- src/controllers/ChannelController.ts
+++ src/controllers/ChannelController.ts
@@ -1,11 +1,19 @@
 import * as web from 'express-decorators'
 import { Response, Request } from '@tinyhttp/app'
-import { DMChannel, Group } from '../structures'
+import { ChannelTypes, DMChannel, Group } from '../structures'
 import { HTTPError } from '../errors'
 import { getaway } from '../server'
+import { validator } from '../utils'
+import config from '../../config'
 
 @web.basePath('/channels/@me')
 export class ChannelController {
+    checks = {
+        createGroup: validator.compile({
+            name: { type: 'string' }
+        })
+    }
+
     @web.get('/')
     async fetchChannels(req: Request, res: Response): Promise<void> {
         const [dms, groups] = await Promise.all([
@@ -19,6 +27,36 @@ export class ChannelController {
             })
         ])
         res.json([...dms, ...groups])
+    }
+
+    @web.post('/')
+    async createGroup(req: Request, res: Response): Promise<void> {
+        const valid = this.checks.createGroup(req.body)
+
+        if (valid !== true) {
+            return void res.status(400).send(valid)
+        }
+
+        const groupCount = await Group.count({
+            deleted: false,
+            recipients: req.user._id
+        })
+
+        if (groupCount >= config.max.user.groups) {
+            return void res.status(403).send(new HTTPError('MAXIMUM_GROUPS'))
+        }
+
+        const group = Group.from({
+            name: req.body.name,
+            ownerId: req.user._id,
+            recipients: [req.user._id]
+        })
+
+        await group.save()
+
+        getaway.emit('CHANNEL_CREATE', group)
+
+        res.json(group)
     }
 
 
@@ -38,8 +76,14 @@ export class ChannelController {
             return void res.status(404).send(new HTTPError('UNKNOWN_CHANNEL'))
         }
 
+        if (channel.type === ChannelTypes.GROUP && channel.ownerId !== req.user._id && !channel.recipients.some((id) => id === req.user._id)) {
+            return void res.status(400).send(new HTTPError('MISSING_ACCESS'))
+        }
+
         res.json(channel)
     }
+
+
 
     @web.route('delete', '/:channelId')
     async deleteChannel(req: Request, res: Response): Promise<void> {
@@ -55,6 +99,10 @@ export class ChannelController {
 
         if (!channel) {
             return void res.status(404).send(new HTTPError('UNKNOWN_CHANNEL'))
+        }
+
+        if (channel.type === ChannelTypes.GROUP && channel.ownerId !== req.user._id) {
+            return void res.status(400).send(new HTTPError('MISSING_ACCESS'))
         }
 
         await channel.save({ deleted: true })
