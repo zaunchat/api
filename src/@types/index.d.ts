a04028925fe9f5203a10317e2c4e751e8e8fb676
TheMaestro0 <abdovc012@gmail.com>
2021-09-12 20:35:59 +0200

    feat: add more types to Request

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,9 +1,13 @@
 import { SyncCheckFunction, AsyncCheckFunction } from 'fastest-validator'
-import { User } from '../structures'
+import { User, Server, Channel } from '../structures'
+import { Permissions } from '../utils'
 
 declare module '@tinyhttp/app' {
     interface Request {
         user: User
+        server: Server
+        channel: Channel
+        permissions: Permissions
         check(fn: SyncCheckFunction | AsyncCheckFunction): boolean
     }
 
