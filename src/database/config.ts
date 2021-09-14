3b8fef6cc89112417bc429c27f7e725fd65a47db
TheMaestro0 <abdovc012@gmail.com>
2021-09-14 02:35:16 +0200

    chore(deps): remove @mikro-orm/reflecion

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,18 +1,15 @@
 import { Configuration, Options } from '@mikro-orm/core'
-import { TsMorphMetadataProvider } from '@mikro-orm/reflection'
 import * as Subscribers from '../subscribers'
+import { User, Message, Channel, Server, Session, Member, Role, Invite } from '../structures'
 import { RedisCacheAdapter } from './redis'
 import config from '../../config'
 import ms from 'ms'
-
-const entities = ['Message', 'User', 'Server', 'Member', 'Channel', 'Role', 'Invite'].join(',')
 
 
 const options: Options = {
 	clientUrl: config.database.uri,
 	type: config.database.type as keyof typeof Configuration.PLATFORMS,
-	entities: [`./dist/src/structures/{${entities}}.js`],
-	entitiesTs: [`./src/structures/{${entities}}.ts`],
+	entities: [User, Message, Channel, Server, Session, Member, Role, Invite],
 	subscribers: Object.values(Subscribers).map(Subscriber => new Subscriber()),
 	dbName: config.database.name,
 	debug: false,
@@ -23,8 +20,7 @@ const options: Options = {
 	resultCache: {
 		adapter: RedisCacheAdapter,
 		expiration: ms('5 second')
-	},
-	metadataProvider: TsMorphMetadataProvider
+	}
 }
 
 export default options
