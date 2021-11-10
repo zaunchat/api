1451f6819d6401d18b6cb6b4e411f656cc6cf46b
TheMaestroo <61483023+TheMaestro1s@users.noreply.github.com>
2021-11-10 18:37:51 +0000

    feat: Remove non-needed try/catch

--- src/index.ts
+++ src/index.ts
@@ -18,19 +18,17 @@ if (cluster.isPrimary) {
 	})
 } else {
 	(async () => {
-		try {
-			console.log('Connecting to database...')
+		console.log('Connecting to database...')
 
-			await db.connect()
+		await db.connect()
 
-			console.log('Connected to Database')
+		console.log('Connected to Database')
 
-			server.listen(config.port, () => console.log(`Server running on port: ${config.port}`))
-		} catch (err) {
-			console.error(err)
-			process.exit(-1)
-		}
-	})()
+		server.listen(config.port, () => console.log(`Server running on port: ${config.port}`))
+	})().catch(err => {
+		console.error(err)
+		process.exit(-1)
+	})
 }
 
 
