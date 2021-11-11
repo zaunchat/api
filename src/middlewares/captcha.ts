98c12fa0b52cb1b57069ba77fb34288f45bdc252
TheMaestroo <61483023+TheMaestro1s@users.noreply.github.com>
2021-11-11 19:47:36 +0000

    fix: Spell check

--- src/middlewares/captcha.ts
+++ src/middlewares/captcha.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from '@tinyhttp/app'
 import { HTTPError } from '../errors'
 import config from '../../config'
 
-interface CatpchaOptions {
+interface CaptchaOptions {
     required: `/${string}`[]
 }
 
-export const captcha = (options: CatpchaOptions) => async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
+export const captcha = (options: CaptchaOptions) => async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
     if (config.captcha.enabled && options.required.some((p) => req.path.includes(p))) {
         if (!req.body.captcha_key) {
             throw new HTTPError('FAILED_CAPTCHA')
