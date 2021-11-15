57fa32cd97ab92b7a1be6fdf4e33a4fee3749ee6
TheMaestroo <abdulrahman.8alah@gmail.com>
2021-11-15 16:56:19 +0000

    it should working.

--- src/getaway/events/Authenticate.ts
+++ src/getaway/events/Authenticate.ts
@@ -1,5 +1,5 @@
 import { Payload, WSCloseCodes, WSCodes, WSEvents } from '../Constants'
-import { Channel, User, PresenceStatus, ChannelTypes } from '../../structures'
+import { Channel, User } from '../../structures'
 import { Socket } from '../Socket'
 
 
@@ -9,19 +9,13 @@ export const Authenticate = async (socket: Socket, data: Payload): Promise<void>
     }
 
     const auth = (data.data ?? {}) as {
-        userid: string,
+        type: 'user',
         token: string
     }
 
-    const user = auth.user_id && auth.token ? await User.findOne({
-        id: auth.user_id,
-        verified: true
-    }, {
-        fields: ['_id', 'avatar', 'username', 'badges', 'email', 'relations', 'servers', 'sessions'],
-        populate: ['servers']
-    }) : null
+    const user = await User.fetchByToken(auth.token)
 
-    if (!user?.sessions?.getItems().some(session => session.token === auth.token)) {
+    if (!user) {
         return socket.close(WSCloseCodes.AUTHENTICATED_FAILED)
     }
 
@@ -30,31 +24,15 @@ export const Authenticate = async (socket: Socket, data: Payload): Promise<void>
 
     await socket.send({ code: WSCodes.AUTHENTICATED })
 
-    const servers = user.servers.getItems()
+    const servers = await user.fetchServers()
     const serverIDs = servers.map(s => s.id)
 
     const [
         users,
         channels
     ] = await Promise.all([
-        User.find({
-            id: {
-                $in: Array.from(user.relations.keys())
-            }
-        }, { public: true }),
-        Channel.find({
-            $or: [{
-                type: ChannelTypes.DM,
-                recipients: user.id
-            }, {
-                type: ChannelTypes.GROUP,
-                recipients: user.id
-            }, {
-                serverid: {
-                    $in: serverIDs
-                }
-            }]
-        })
+        user.fetchRelations(),
+        Channel.find(`server_id IN ${serverIDs} OR recipients::jsonb ? ${user.id}`)
     ])
 
     const clientUser = {
@@ -76,18 +54,10 @@ export const Authenticate = async (socket: Socket, data: Payload): Promise<void>
         data: readyData
     })
 
-
     await socket.subscribe(...[
         [user.id],
         [...user.relations.keys()],
         serverIDs,
         channels.map(c => c.id)
-    ].flat(4))
-
-    if (!user.presence.ghost_mode) await user.save({
-        presence: {
-            ghost_mode: user.presence.ghost_mode,
-            status: PresenceStatus.ONLINE
-        }
-    })
+    ].flat(4) as ID[])
 }
