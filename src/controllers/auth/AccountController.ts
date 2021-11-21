f1d841c2127e2111874bff8ed86a4c98111e99dc
TheMaestro <61483023+TheMaestro1s@users.noreply.github.com>
2021-11-21 23:52:01 +0200

    fix(AccountController): Currect url response

--- src/controllers/auth/AccountController.ts
+++ src/controllers/auth/AccountController.ts
@@ -9,95 +9,95 @@ import argon2 from 'argon2'
 
 @web.basePath('/auth/accounts')
 export class AccountController {
-	@web.post('/login')
-	async login(req: Request, res: Response): Promise<void> {
-		req.check(LoginUserSchema)
+  @web.post('/login')
+  async login(req: Request, res: Response): Promise<void> {
+    req.check(LoginUserSchema)
 
-		const { email, password } = req.body
+    const { email, password } = req.body
 
-		if (!is.email(email)) {
-			throw new HTTPError('INVALID_EMAIL')
-		}
+    if (!is.email(email)) {
+      throw new HTTPError('INVALID_EMAIL')
+    }
 
-		const user = await User.findOne(`email = ${email}`)
+    const user = await User.findOne(`email = ${email}`)
 
-		if (!user.verified) {
-			throw new HTTPError('USER_NOT_VERIFIED')
-		}
+    if (!user.verified) {
+      throw new HTTPError('USER_NOT_VERIFIED')
+    }
 
-		if (!await argon2.verify(password, user.password)) {
-			throw new HTTPError('INVALID_PASSWORD')
-		}
+    if (!await argon2.verify(password, user.password)) {
+      throw new HTTPError('INVALID_PASSWORD')
+    }
 
-		const session = Session.from({
-			user_id: user.id
-		})
+    const session = Session.from({
+      user_id: user.id
+    })
 
-		await session.save()
+    await session.save()
 
-		res.json({
-			token: session.token,
-			id: user.id
-		})
-	}
+    res.json({
+      token: session.token,
+      id: user.id
+    })
+  }
 
-	@web.post('/register')
-	async register(req: Request, res: Response): Promise<void> {
-		req.check(CreateUserSchema)
+  @web.post('/register')
+  async register(req: Request, res: Response): Promise<void> {
+    req.check(CreateUserSchema)
 
-		const { username, password } = req.body
+    const { username, password } = req.body
 
-		if (!is.email(req.body.email)) {
-			throw new HTTPError('INVALID_EMAIL')
-		}
+    if (!is.email(req.body.email)) {
+      throw new HTTPError('INVALID_EMAIL')
+    }
 
-		const exists = await User.findOne(`email = ${email} OR username = ${username}`, ['username', 'email']).catch(() => null)
+    const exists = await User.findOne(`email = ${email} OR username = ${username}`, ['username', 'email']).catch(() => null)
 
 
-		if (exists) {
-			if (username === exists.username) {
-				throw new HTTPError('USERNAME_TAKEN')
-			} else {
-				throw new HTTPError('EMAIL_ALREADY_IN_USE')
-			}
-		}
+    if (exists) {
+      if (username === exists.username) {
+        throw new HTTPError('USERNAME_TAKEN')
+      } else {
+        throw new HTTPError('EMAIL_ALREADY_IN_USE')
+      }
+    }
 
-		const user = await User.from({
-			username,
-			email: req.body.email,
-			password: await argon2.hash(password),
-			verified: !config.smtp.enabled
-		})
-		
-		await user.save()
+    const user = User.from({
+      username,
+      email: req.body.email,
+      password: await argon2.hash(password),
+      verified: !config.smtp.enabled
+    })
 
-		if (!config.smtp.enabled) {
-			return void res.redirect(`https://${config.endpoints.main}/auth/login`)
-		}
+    await user.save()
 
-		try {
-			await email.send(user)
-			res.json({ message: 'Check your email' })
-		} catch (err) {
-			await user.delete()
-			throw err
-		}
-	}
+    if (!config.smtp.enabled) {
+      return void res.redirect(`${config.endpoints.main}/auth/login`)
+    }
 
-	@web.get('/verify/:user_id/:code')
-	async verify(req: Request, res: Response): Promise<void> {
-		const { user_id, code } = req.params as { user_id: ID; code: string }
+    try {
+      await email.send(user)
+      res.json({ message: 'Check your email' })
+    } catch (err) {
+      await user.delete()
+      throw err
+    }
+  }
 
-		const verified = await email.verify(user_id, code)
+  @web.get('/verify/:user_id/:code')
+  async verify(req: Request, res: Response): Promise<void> {
+    const { user_id, code } = req.params as { user_id: ID; code: string }
 
-		if (!verified) {
-			throw new HTTPError('UNKNOWN_TOKEN')
-		}
+    const verified = await email.verify(user_id, code)
 
-		const user = await User.findOne(`id = ${user_id}`)
+    if (!verified) {
+      throw new HTTPError('UNKNOWN_TOKEN')
+    }
 
-		await user.update({ verified: true })
+    const user = await User.findOne(`id = ${user_id}`)
 
-		res.redirect(`${config.endpoints.main}/auth/login`)
-	}
+    await user.update({ verified: true })
+
+    res.redirect(`${config.endpoints.main}/auth/login`)
+  }
 }
