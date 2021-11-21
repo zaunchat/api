296ba4af9a3c266c0b1d63d6a5a1cde2cc96eb09
TheMaestro <61483023+TheMaestro1s@users.noreply.github.com>
2021-11-21 23:51:00 +0200

    fix(UserController#fetchOne): Hide private fields

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,49 +1,49 @@
 import * as web from 'express-decorators'
 import { Response, Request } from '@tinyhttp/app'
-import { Channel, ChannelTypes, User } from '../structures'
+import { Channel, ChannelTypes, PUBLIC_USER_PROPS, User } from '../structures'
 
 
 @web.basePath('/users')
 export class UserController {
-    @web.get('/:user_id')
-    async fetchOne(req: Request, res: Response): Promise<void> {
-        const user = await User.findOne(`id = ${req.params.user_id}`)
-        res.json(user)
+  @web.get('/:user_id')
+  async fetchOne(req: Request, res: Response): Promise<void> {
+    const user = await User.findOne(`id = ${req.params.user_id}`, PUBLIC_USER_PROPS)
+    res.json(user)
+  }
+
+  @web.get('/@me/relationships')
+  async fetchRelationships(req: Request, res: Response): Promise<void> {
+    const relationships = await req.user.fetchRelations()
+    res.json(relationships)
+  }
+
+  @web.get('/:user_id/dm')
+  async openDM(req: Request, res: Response): Promise<void> {
+    const { user_id } = req.params as Record<string, ID>
+    const target = await User.findOne(`id = ${user_id}`)
+    const exists = await Channel.findOne(`type = ${ChannelTypes.DM} AND recipients::jsonb ? ${user_id}`).catch(() => null)
+
+    if (exists) {
+      return void res.json(exists)
     }
 
-    @web.get('/@me/relationships')
-    async fetchRelationships(req: Request, res: Response): Promise<void> {
-        const relationships = await req.user.fetchRelations()
-        res.json(relationships)
-    }
+    const dm = Channel.from({
+      type: ChannelTypes.DM,
+      recipients: [req.user.id, target.id]
+    })
 
-    @web.get('/:user_id/dm')
-    async openDM(req: Request, res: Response): Promise<void> {
-        const { user_id } = req.params as Record<string, ID>
-        const target = await User.findOne(`id = ${user_id}`)
-        const exists = await Channel.findOne(`type = ${ChannelTypes.DM} AND recipients::jsonb ? ${user_id}`).catch(() => null)
+    await dm.save()
 
-        if (exists) {
-            return void res.json(exists)
-        }
+    res.json(dm)
+  }
 
-        const dm = Channel.from({
-            type: ChannelTypes.DM,
-            recipients: [req.user.id, target.id]
-        })
-
-        await dm.save()
-
-        res.json(dm)
-    }
-
-    // TODO: Add better relationship handling.
-    // @web.post('/:user_id/friend')
-    // async friend(req: Request, res: Response): Promise<void> {}
-    // @web.route('delete', '/:user_id/friend')
-    // async unfriend(req: Request, res: Response): Promise<void> {}
-    // @web.post('/:user_id/block')
-    // async block(req: Request, res: Response): Promise<void> {}
-    // @web.route('delete', '/:user_id')
-    // async unblock(req: Request, res: Response): Promise<void> {}
+  // TODO: Add better relationship handling.
+  // @web.post('/:user_id/friend')
+  // async friend(req: Request, res: Response): Promise<void> {}
+  // @web.route('delete', '/:user_id/friend')
+  // async unfriend(req: Request, res: Response): Promise<void> {}
+  // @web.post('/:user_id/block')
+  // async block(req: Request, res: Response): Promise<void> {}
+  // @web.route('delete', '/:user_id')
+  // async unblock(req: Request, res: Response): Promise<void> {}
 }
