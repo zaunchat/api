8a55dd084afadb4896e396e46b31d9af5188a40f
TheMaestro <61483023+TheMaestro1s@users.noreply.github.com>
2021-11-21 23:32:55 +0200

    feat(Logger): Add colors

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,13 +1,24 @@
+import kleur from 'kleur'
+
 const time = () => new Date().toTimeString().slice(0, 8)
 
+const color = (mode: 'error' | 'info' | 'warn' | 'log'): string => {
+  switch (mode) {
+    case 'error': return kleur.red('ERROR')
+    case 'warn': return kleur.yellow('WARN')
+    case 'info': return kleur.blue('INFO')
+    case 'log': return kleur.green('LOG')
+  }
+}
+
 class Logger {
-  private _log(data: unknown[], _color = 'green'): this {
-    console.log(`[${time()}]:`, ...data)
+  private _log(data: unknown[], mode: string): this {
+    console.log(`[${time()}] [${color(mode as 'log')}]:`, ...data)
     return this
   }
 
   log(...data: unknown[]): this {
-    return this._log(data)
+    return this._log(data, 'log')
   }
 
   error(...data: unknown[]): this {
