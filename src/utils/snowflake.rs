99942679c1812d2a1094e8035459673eedc08249
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-06-05 10:36:14 +0000

    Add snowflake util

---
+++ src/utils/snowflake.rs
@@ -0,0 +1,11 @@
+use snowflake::SnowflakeIdGenerator;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+lazy_static! {
+    static ref ITCHAT_EPOCH: SystemTime = UNIX_EPOCH + Duration::from_millis(1609459200000);
+}
+
+pub fn generate_id() -> i64 {
+    let mut generator = SnowflakeIdGenerator::with_epoch(1, 1, *ITCHAT_EPOCH);
+    generator.generate()
+}
