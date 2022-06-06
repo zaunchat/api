909cb21c1d8e12b5d2dadefb172cb277bfaa7710
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-06-06 01:15:55 +0000

    feat(Base): Add #delete

--- src/structures/base.rs
+++ src/structures/base.rs
@@ -5,39 +5,30 @@ use rbatis::wrapper::Wrapper;
 use rocket::serde::DeserializeOwned;
 
 #[async_trait]
-pub trait Base {
+pub trait Base: CRUDTable + DeserializeOwned {
     async fn find<F>(query: F) -> Vec<Self>
     where
-        Self: CRUDTable + DeserializeOwned,
         F: Send + Fn(Wrapper) -> Wrapper,
     {
         let query = query(db.new_wrapper());
         db.fetch_list_by_wrapper(query).await.unwrap()
     }
 
-    async fn find_one<F>(query: F) -> Self
+    async fn find_one<F>(query: F) -> Option<Self>
     where
-        Self: CRUDTable + DeserializeOwned,
         F: Send + Fn(Wrapper) -> Wrapper,
     {
         let query = query(db.new_wrapper());
-        db.fetch_by_wrapper(query).await.unwrap()
+        db.fetch_by_wrapper(query).await.ok()
     }
 
-    async fn save(&self)
-    where
-        Self: CRUDTable + DeserializeOwned,
-    {
-        db.save(&self, &[]).await.unwrap();
+    async fn save(&self) {
+        db.save(&self, &[])
+            .await
+            .expect("Couldn't save this target");
     }
 
-    // TODO: Implement that in here.
-    async fn delete(&self) -> bool {
-        false
+    async fn delete(&self, id: i64) -> bool {
+        db.remove_by_column::<Self, &i64>("id", &id).await.is_ok()
     }
-
-    // async fn delete(&self) -> bool
-    // {
-    //     db.remove_by_column("id", &self.id).await.is_ok()
-    // }
 }
