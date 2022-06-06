9b13fe97d70dbd02eb2b13584993c5dedce366fb
TheMaestroo <abdulrahman.8alah@gmail.com>
2022-06-06 01:15:17 +0000

    feat(routes): Add accounts

--- src/routes/auth/accounts.rs
+++ src/routes/auth/accounts.rs
@@ -1,23 +1,69 @@
-use rocket::serde::{Deserialize, json::Json};
+use crate::structures::{Base, User};
+use crate::utils::error::{Error, Result};
+use crate::SMTP_ENABLED;
+use argon2::Config;
+use nanoid::nanoid;
+use rocket::serde::{json::Json, Deserialize};
+use validator::Validate;
 
-#[derive(Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct LoginUserSchema {
-    email: String,
-    password: String
+#[derive(Debug, Deserialize, Validate, Clone, Copy)]
+pub struct RegisterSchema<'r> {
+    #[validate(length(min = 3, max = 32))]
+    pub username: &'r str,
+    #[validate(length(min = 8, max = 32))]
+    pub password: &'r str,
+    #[validate(email)]
+    pub email: &'r str,
 }
 
-#[post("/login")]
-pub fn login(body: LoginUserSchema) {
+#[post("/register", data = "<data>")]
+pub async fn register(data: Json<RegisterSchema<'_>>) -> Result<Json<User>> {
+    let data = data.into_inner();
 
-}
+    data.validate()
+        .map_err(|error| Error::InvalidBody { error })?;
 
-#[post("/register")]
-pub fn register() {
+    let email_in_use = User::find_one(|q| q.eq("email", &data.email))
+        .await
+        .is_some();
 
+    if email_in_use {
+        return Err(Error::EmailAlreadyInUse);
+    }
+
+    let config = Config::default();
+    let salt = nanoid!(24);
+    let hashed_password = argon2::hash_encoded(
+        data.password.to_string().as_bytes(),
+        salt.as_bytes(),
+        &config,
+    )
+    .unwrap();
+
+    let mut user = User::new(data.username.into(), data.email.into(), hashed_password);
+
+    if *SMTP_ENABLED {
+        todo!("Send email verification")
+    } else {
+        user.verified = true;
+    }
+
+    user.save().await;
+
+    Ok(Json(user))
 }
 
 #[get("/verify/<user_id>/<code>")]
-pub fn verify(user_id: i64, code: &str) {
+pub async fn verify(user_id: i64, code: &str) -> Result<()> {
+    let user = User::find_one(|q| q.eq("id", &user_id).eq("verified", false)).await;
 
+    // TODO: Check verification code.
+
+    if let Some(mut user) = user {
+        user.verified = true;
+        user.save().await;
+        Ok(())
+    } else {
+        Err(Error::AccountNotFound)
+    }
 }
