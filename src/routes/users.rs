d2173e645e428224f379afd955f3619b5fef4241
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-06-06 15:08:56 +0200

    feat(routes): Add basic user routes

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,3 +1,24 @@
+use crate::structures::{Base, User};
+use crate::utils::error::*;
+use rocket::serde::{json::Json};
+
+#[get("/@me")]
+fn fetch_me(user: User) -> Json<User> {
+    Json(user.to_public())
+}
+
+#[get("/<user_id>")]
+async fn fetch_user(user_id: u64) -> Result<Json<User>> {
+    let user = User::find_one_by_id(user_id).await;
+
+    if let Some(user) = user {
+        return Ok(Json<(user.to_public()));
+    }
+
+    Err(Error::UnknownUser)
+}
+
+
 pub fn routes() -> Vec<rocket::Route> {
-    routes![]
+    routes![fetch_me, fetch_user]
 }
