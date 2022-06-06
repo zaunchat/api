f643ba27cac91179ac691e3e17f9cb8593d1964e
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-06-06 15:10:35 +0200

    feat(fairings): Add rate limiter

--- src/fairings/ratelimit.rs
+++ src/fairings/ratelimit.rs
@@ -1,16 +1,35 @@
 use crate::utils::error::*;
 use rocket::{
     fairing::{Fairing, Info, Kind},
-    http::{uri::Origin, Method},
+    http::{uri::Origin, Method, Status},
     outcome::Outcome,
     Data, Request, Route,
 };
 
+use governor::{
+    clock::{Clock, DefaultClock},
+    state::keyed::DefaultKeyedStateStore,
+    Quota, RateLimiter,
+};
 
-pub struct RateLimiter;
+lazy_static! {
+    static ref CLOCK: DefaultClock = DefaultClock::default();
+}
+
+pub struct RateLimit {
+    limiter: RateLimiter
+}
+
+impl RateLimit {
+    pub fn new(seconds: u32) -> Self {
+        Self {
+            limiter: RateLimiter::<String, _, _>::keyed(Quota::per_second(seconds))
+        }
+    }
+}
 
 #[rocket::async_trait]
-impl Fairing for RateLimiter<'static> {
+impl Fairing for RateLimit<'static> {
     fn info(&self) -> Info {
         Info {
             name: "RateLimiter",
@@ -19,16 +38,27 @@ impl Fairing for RateLimiter<'static> {
     }
 
     async fn on_request(&self, req: &mut Request<'_>, _: &mut Data<'_>) {
-        todo!();
-        req.set_method(Method::Get);
-        req.set_uri(Origin::parse("/rate_limited").unwrap())
+        let key = req.real_ip();
+        let path = req.uri().path().as_str();
+
+        match self.limiter.check_key(&key) {
+            Ok(_) => {
+                log::debug!("Allowing {} to access {}", key, path);
+            },
+            Err(negative) => {
+                let wait_time = negative.wait_time_from(CLOCK.now());
+                log::debug!("Blocking {} for {} seconds", key, wait_time.as_seconds());
+                req.set_method(Method::Get);
+                req.set_uri(Origin::parse("/ratelimit").unwrap())
+            }
+        }
     }
 }
 
 
-#[get("/rate_limited")]
-fn rate_limited() -> String {
-    "Rate limited".into()
+#[get("/ratelimit")]
+fn rate_limit() -> Status {
+    Status::TooManyRequests
 }
 
 pub fn routes() -> Vec<Route> {
