7c93fa60231b7bed043117b630fa6ca01a5a1a22
TheMaestroo <abdulrahman.8alah@gmail.com>
2022-06-07 05:46:50 +0000

    feat(guards): Add Ref guard

--- src/guards/mod.rs
+++ src/guards/mod.rs
@@ -1,3 +1,4 @@
 pub mod captcha;
 pub mod user;
+pub mod r#ref;
 pub use captcha::Captcha;
---
+++ src/guards/ref.rs
@@ -0,0 +1,35 @@
+use crate::structures::{Base, User, Channel};
+use rocket::request::FromParam;
+use crate::utils::error::*;
+
+pub struct Ref(u64);
+
+impl Ref {
+    pub fn from_unchecked(id: String) -> Ref {
+        Ref(id.parse().unwrap())
+    }
+
+    pub async fn user(&self) -> Result<User> {
+        let user = User::find_one_by_id(self.0).await;
+        match user {
+            Some(u) => Ok(u),
+            _ => Err(Error::UnknownUser)
+        }
+    }
+
+    pub async fn channel(&self) -> Result<Channel> {
+        let channel = Channel::find_one_by_id(self.0).await;
+        match channel {
+            Some(c) => Ok(c),
+            _ => Err(Error::UnknownChannel)
+        }
+    }
+}
+
+impl<'r> FromParam<'r> for Ref {
+    type Error = &'r str;
+
+    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+        Ok(Ref::from_unchecked(param.into()))
+    }
+}
