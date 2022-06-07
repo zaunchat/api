c7cf19a6c89893fa04bae62d3cd249d35d272715
TheMaestroo <abdulrahman.8alah@gmail.com>
2022-06-07 06:34:41 +0000

    feat(routes): Add messages route

--- src/routes/messages.rs
+++ src/routes/messages.rs
@@ -1,3 +1,107 @@
+use crate::guards::r#ref::Ref;
+use crate::structures::*;
+use crate::utils::error::*;
+use crate::utils::permissions::*;
+use rocket::serde::{json::Json, Deserialize};
+use validator::Validate;
+
+#[derive(Deserialize, Validate)]
+struct CreateMessageSchema<'r> {
+    channel_id: u64,
+    #[validate(length(min = 1, max = 2000))]
+    content: &'r str,
+}
+
+#[post("/", data = "<data>")]
+async fn send(user: User, data: Json<CreateMessageSchema<'_>>) -> Result<Json<Message>> {
+    let data = data.into_inner();
+
+    data.validate()
+        .map_err(|error| Error::InvalidBody { error })?;
+
+    let p = Permissions::fetch(&user, None, data.channel_id.into()).await?;
+
+    if !p.contains(Permissions::VIEW_CHANNEL) || !p.contains(Permissions::SEND_MESSAGES) {
+        return Err(Error::MissingPermissions);
+    }
+
+    let mut msg = Message::new(data.channel_id, user.id);
+
+    // TODO: Add more fields
+    msg.content = Some(data.content.into());
+
+    if msg.is_empty() {
+        return Err(Error::EmptyMessage);
+    }
+
+    msg.save().await;
+
+    Ok(Json(msg))
+}
+
+#[derive(Deserialize, Validate)]
+struct EditMessageSchema<'r> {
+    #[validate(length(min = 1, max = 2000))]
+    content: &'r str,
+}
+
+#[patch("/<target>", data = "<data>")]
+async fn edit(user: User, target: Ref, data: Json<EditMessageSchema<'_>>) -> Result<Json<Message>> {
+    let data = data.into_inner();
+
+    data.validate()
+        .map_err(|error| Error::InvalidBody { error })?;
+
+    let mut msg = target.message().await?;
+
+    if msg.author_id != user.id {
+        return Err(Error::MissingPermissions);
+    }
+
+    let p = Permissions::fetch(&user, None, msg.channel_id.into()).await?;
+
+    if !p.contains(Permissions::VIEW_CHANNEL) {
+        return Err(Error::MissingPermissions);
+    }
+
+    msg.content = Some(data.content.into());
+    msg.update().await;
+
+    Ok(Json(msg))
+}
+
+
+#[delete("/<target>")]
+async fn delete(user: User, target: Ref) -> Result<()> {
+    let msg = target.message().await?;
+    let p = Permissions::fetch(&user, None, Some(msg.channel_id)).await?;
+
+    if !p.contains(Permissions::VIEW_CHANNEL) {
+        return Err(Error::MissingPermissions);
+    }
+
+    if msg.author_id != user.id && !p.contains(Permissions::MANAGE_MESSAGES) {
+        return Err(Error::MissingPermissions);
+    }
+
+    msg.delete(msg.id).await;
+
+    Ok(())
+}
+
+#[get("/<target>")]
+async fn fetch_one(user: User, target: Ref) -> Result<Json<Message>> {
+    let msg = target.message().await?;
+    let p = Permissions::fetch(&user, None, msg.channel_id.into()).await?;
+
+    if !p.contains(Permissions::VIEW_CHANNEL) || !p.contains(Permissions::READ_MESSAGE_HISTORY) {
+        return Err(Error::MissingPermissions);
+    }
+
+    Ok(Json(msg))
+}
+
+
 pub fn routes() -> Vec<rocket::Route> {
-    routes![]
+    routes![send, edit, delete, fetch_one]
 }
