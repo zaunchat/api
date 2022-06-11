51f4205cd1963dcc7f4e7190b48430dd0e9d8ddc
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-06-11 12:43:42 +0000

    feat: Add trust cloudflare option

--- src/config.rs
+++ src/config.rs
@@ -1,34 +1,27 @@
 use std::env;
 
-fn is_true(v: Result<String, env::VarError>) -> bool {
-    if let Ok(mut v) = v {
-        v = v.to_lowercase();
-        return v == "true" || v == "yes";
-    }
-    false
+fn is_true(mut v: String) -> bool {
+    v = v.to_lowercase();
+    return v == "true" || v == "yes";
 }
 
-fn default(v: Result<String, env::VarError>, default_value: &str) -> String {
-    if let Ok(v) = v {
-        v
-    } else {
-        default_value.to_string()
-    }
+macro_rules! get {
+    ($key:expr) => {{
+        env::var($key).expect(format!("{} is required", $key).as_str())
+    }};
+    ($key:expr, $default: expr) => {{
+        env::var($key).unwrap_or($default.to_string())
+    }};
 }
 
 lazy_static! {
-    pub static ref DATABASE_URI: String =
-        env::var("DATABASE_URI").expect("DATABASE_URI is required");
-    pub static ref CAPTCHA_ENABLED: bool = is_true(env::var("CAPTCHA_ENABLED"));
-    pub static ref CAPTCHA_TOKEN: String =
-        env::var("CAPTCHA_TOKEN").expect("CAPTCHA_TOKEN is required");
-    pub static ref CAPTCHA_KEY: String = env::var("CAPTCHA_KEY").expect("CAPTCHA_KEY is required");
-    pub static ref PORT: String = default(env::var("PORT"), "8080");
-    pub static ref EMAIL_VERIFICATION: bool = is_true(env::var("EMAIL_VERIFICATION"));
-    pub static ref REQUIRE_INVITE_TO_REGISTER: bool =
-        is_true(env::var("REQUIRE_INVITE_TO_REGISTER"));
-    pub static ref INVITEATION_CODE: String =
-        env::var("INVITEATION_CODE").expect("INVITEATION_CODE is required");
-    pub static ref SENDINBLUE_API_KEY: String =
-        env::var("SENDINBLUE_API_KEY").expect("SENDINBLUE_API_KEY is required");
+    pub static ref DATABASE_URI: String = get!("DATABASE_URI");
+    pub static ref CAPTCHA_ENABLED: bool = is_true(get!("CAPTCHA_ENABLED", "false"));
+    pub static ref CAPTCHA_TOKEN: String = get!("CAPTCHA_TOKEN");
+    pub static ref CAPTCHA_KEY: String = get!("CAPTCHA_KEY");
+    pub static ref PORT: String = get!("PORT", "8080");
+    pub static ref EMAIL_VERIFICATION: bool = is_true(get!("EMAIL_VERIFICATION", "false"));
+    pub static ref REQUIRE_INVITE_TO_REGISTER: bool = is_true(get!("REQUIRE_INVITE_TO_REGISTER", "false"));
+    pub static ref SENDINBLUE_API_KEY: String = get!("SENDINBLUE_API_KEY");
+    pub static ref TRUST_CLOUDFLARE: bool = is_true(get!("TRUST_CLOUDFLARE", "false"));
 }
