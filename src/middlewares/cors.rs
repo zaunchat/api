778b19541c4a6619f2faea510aab4e59291f69b6
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-06-14 11:55:34 +0000

    fix(cors): explicitly allowed headers

--- src/middlewares/cors.rs
+++ src/middlewares/cors.rs
@@ -1,13 +1,22 @@
+use axum::http::header::*;
 use axum::http::Method;
 use tower_http::cors::CorsLayer;
 
 pub fn handle() -> CorsLayer {
-    CorsLayer::new().allow_methods([
-        Method::GET,
-        Method::DELETE,
-        Method::PATCH,
-        Method::POST,
-        Method::OPTIONS,
-        Method::HEAD,
-    ])
+    CorsLayer::new()
+        .allow_methods([
+            Method::GET,
+            Method::DELETE,
+            Method::PATCH,
+            Method::POST,
+            Method::OPTIONS,
+            Method::HEAD,
+        ])
+        .allow_headers([
+            ACCEPT,
+            AUTHORIZATION,
+            CONTENT_TYPE,
+            CONTENT_LENGTH,
+            HeaderName::from_static("X-Captcha-Key"),
+        ])
 }
