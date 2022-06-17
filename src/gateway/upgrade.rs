3e465d8f4f46e0d19efc4a2ad9eda57be02dc398
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-06-17 18:54:40 +0000

    feat: handle outcoming data

--- src/gateway/upgrade.rs
+++ src/gateway/upgrade.rs
@@ -4,6 +4,8 @@ use axum::{
     headers,
     response::IntoResponse,
 };
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 pub async fn upgrade(
     ws: WebSocketUpgrade,
@@ -11,7 +13,10 @@ pub async fn upgrade(
 ) -> impl IntoResponse {
     ws.on_upgrade(|stream| async {
         let config = SocektConfig::new();
-        let mut client = SocketClient::new(stream, config).await;
-        client.handle().await
+        let client = Arc::new(Mutex::new(SocketClient::new(stream, config).await));
+        tokio::join!(
+            SocketClient::handle_incoming(client.clone()),
+            SocketClient::handle_outcoming(client)
+        );
     })
 }
