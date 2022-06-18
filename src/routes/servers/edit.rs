3be82f6be4a82e6105c995004d36a72f3846e62c
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-06-18 12:35:06 +0200

    feat: Add server edit route

--- src/routes/servers/edit.rs
+++ src/routes/servers/edit.rs
@@ -1,7 +1,35 @@
 use crate::extractors::*;
 use crate::structures::*;
 use crate::utils::*;
+use serde::Deserialize;
+use validator::Validate;
+use crate::gateway::*;
 
-pub async fn edit(Extension(_user): Extension<User>, Path(_server_id): Path<u64>) -> Result<()> {
-    todo!()
+
+#[derive(Deserialize, Validate, OpgModel)]
+pub struct EditServerOptions {
+    #[validate(length(min = 1, max = 50))]
+    name: String,
+}
+
+pub async fn edit(
+    Extension(user): Extension<User>,
+    Path(server_id): Path<u64>,
+    ValidatedJson(data): ValidatedJson<EditServerOptions>
+) -> Result<Json<Server>> {
+    user.member_of(server_id).await?;
+
+    Permissions::fetch(&user, server_id.into(), None).await?.has(Permissions::MANAGE_SERVER)?;
+
+    let server = server_id.server().await?;
+
+    if let Some(name) = data.name {
+        server.name = name;
+    }
+
+    server.update().await;
+
+    publish(server.id, Payload::ServerUpdate(server.clone())).await;
+
+    Ok(Json(server))
 }
