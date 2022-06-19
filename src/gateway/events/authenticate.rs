b5942575ef7c1a20e7684d0c0bfe494e49a40c8a
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-06-19 19:34:17 +0000

    fix: subscribe to server channels

--- src/gateway/events/authenticate.rs
+++ src/gateway/events/authenticate.rs
@@ -23,10 +23,14 @@ pub async fn run(client: &mut SocketClient, payload: Payload) {
 
     let user = user.unwrap();
 
-    client.user_id = user.id;
+    client.user = user.clone().into();
+    client.subscriptions.subscribe(user.id).await.unwrap();
 
-    let channels = user.fetch_channels().await;
+    let mut channels = user.fetch_channels().await;
     let servers = user.fetch_servers().await;
+    let server_ids: Vec<u64> = servers.iter().map(|x| x.id).collect();
+
+    channels.append(&mut Channel::find(|q| q.r#in("server_id", &server_ids)).await);
 
     for server in &servers {
         let permissions = Permissions::fetch(&user, server.id.into(), None)
@@ -37,7 +41,7 @@ pub async fn run(client: &mut SocketClient, payload: Payload) {
     }
 
     for channel in &channels {
-        let permissions = Permissions::fetch(&user, None, channel.id.into())
+        let permissions = Permissions::fetch(&user, channel.server_id, channel.id.into())
             .await
             .unwrap();
         client.subscriptions.subscribe(channel.id).await.unwrap();
@@ -45,14 +49,8 @@ pub async fn run(client: &mut SocketClient, payload: Payload) {
     }
 
     client
-        .subscriptions
-        .subscribe(client.user_id)
-        .await
-        .unwrap();
-
-    client
         .send(Payload::Ready {
-            user: user.clone(),
+            user,
             servers,
             channels,
         })
