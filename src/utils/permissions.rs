b5f556067306e21f6193465261d3b95b09a564d1
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-06-27 00:23:31 +0000

    fix: deserialize badge/permission from string

--- src/utils/permissions.rs
+++ src/utils/permissions.rs
@@ -170,12 +170,18 @@ impl<'de> Visitor<'de> for PermissionsVisitor {
         formatter.write_str("a valid permissions")
     }
 
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        self.visit_u64(v.parse().map_err(E::custom)?)
+    }
+
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
     where
         E: serde::de::Error,
     {
-        let bits = v.parse().map_err(E::custom)?;
-        self.visit_u64(bits)
+        self.visit_u64(v.parse().map_err(E::custom)?)
     }
 
     fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
@@ -201,7 +207,7 @@ impl<'de> Deserialize<'de> for Permissions {
     where
         D: Deserializer<'de>,
     {
-        deserializer.deserialize_i64(PermissionsVisitor)
+        deserializer.deserialize_any(PermissionsVisitor)
     }
 }
 
