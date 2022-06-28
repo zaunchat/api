49b9272933f5b6db0136cc62c497c857a226f8a6
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-06-28 17:03:37 +0000

    fix(cors): Allow origin to any

--- src/middlewares/cors.rs
+++ src/middlewares/cors.rs
@@ -2,10 +2,11 @@ use std::str::FromStr;
 
 use axum::http::header::*;
 use axum::http::Method;
-use tower_http::cors::CorsLayer;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn handle() -> CorsLayer {
     CorsLayer::new()
+        .allow_origin(Any)
         .allow_methods([
             Method::GET,
             Method::DELETE,
