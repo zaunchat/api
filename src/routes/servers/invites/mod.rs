ae10968c30f3718e724213e08db0cacf22ae1955
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-07-04 02:19:00 +0000

    fix: allow member middleware to accept all server-related routes

--- src/routes/servers/invites/mod.rs
+++ src/routes/servers/invites/mod.rs
@@ -7,6 +7,6 @@ pub fn routes() -> axum::Router {
 
     Router::new()
         .route("/", get(fetch::fetch_many))
-        .route("/:invite_id", get(fetch::fetch_one).delete(delete::delete))
+        .route("/:id", get(fetch::fetch_one).delete(delete::delete))
         .layer(middleware::from_fn(member::handle))
 }
