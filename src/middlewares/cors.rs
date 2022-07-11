8389b6e554a190ea7b691b95d812df7d7caa0789
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-07-11 12:12:19 +0000

    fix(cors): allow put method

--- src/middlewares/cors.rs
+++ src/middlewares/cors.rs
@@ -1,25 +1,25 @@
-use std::str::FromStr;
-
 use axum::http::header::*;
 use axum::http::Method;
+use std::str::FromStr;
 use tower_http::cors::{Any, CorsLayer};
 
 pub fn handle() -> CorsLayer {
     CorsLayer::new()
         .allow_origin(Any)
         .allow_methods([
-            Method::GET,
             Method::DELETE,
+            Method::GET,
+            Method::HEAD,
+            Method::OPTIONS,
             Method::PATCH,
             Method::POST,
-            Method::OPTIONS,
-            Method::HEAD,
+            Method::PUT,
         ])
         .allow_headers([
             ACCEPT,
             AUTHORIZATION,
-            CONTENT_TYPE,
             CONTENT_LENGTH,
+            CONTENT_TYPE,
             HeaderName::from_str("X-Captcha-Key").unwrap(),
         ])
 }
