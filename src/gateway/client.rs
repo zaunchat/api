905652f8dcbfff82de6de2cb5bdc070c92ce378d
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-07-11 11:52:05 +0000

    fmt: code cleanup

--- src/gateway/client.rs
+++ src/gateway/client.rs
@@ -1,12 +1,16 @@
-use super::events::*;
 use super::payload::{ClientPayload, Payload};
-use crate::{structures::User, utils::Permissions};
+use super::{events::*, Empty};
+use crate::{
+    structures::User,
+    utils::{Permissions, Ref},
+};
 use axum::extract::ws::{Message, WebSocket};
-use fred::clients::SubscriberClient;
-use futures::{stream::SplitSink, SinkExt};
-use std::collections::HashMap;
-
-use std::sync::Arc;
+use fred::{clients::SubscriberClient, interfaces::PubsubInterface};
+use futures::{
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
+};
+use std::{collections::HashMap, sync::Arc};
 use tokio::sync::Mutex;
 
 pub struct Client {
@@ -17,14 +21,11 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn from(
-        stream: Arc<tokio::sync::Mutex<SplitSink<WebSocket, Message>>>,
-        subscriptions: SubscriberClient,
-    ) -> Self {
+    pub fn new(stream: SplitSink<WebSocket, Message>, subscriptions: SubscriberClient) -> Self {
         Self {
             permissions: Mutex::new(HashMap::new()),
             user: Mutex::new(None),
-            write: stream,
+            write: Arc::new(Mutex::new(stream)),
             subscriptions,
         }
     }
@@ -33,21 +34,137 @@ impl Client {
         self.write.lock().await.send(payload.into()).await
     }
 
-    pub async fn on_message(&self, content: String) {
-        let payload = serde_json::from_str::<ClientPayload>(&content);
+    pub async fn handle_outgoing(&self) {
+        while let Some((channel, payload)) = self.subscriptions.on_message().next().await {
+            let target_id: i64 = channel.parse().unwrap();
+            let user = self.user.lock().await;
+            let user = &*user.as_ref().unwrap();
 
-        if payload.is_err() {
-            log::debug!("Socket sent an invalid body");
-            return;
+            let payload: Payload = serde_json::from_str(&payload.as_string().unwrap()).unwrap();
+            let mut permissions = self.permissions.lock().await;
+            let p = permissions
+                .get(&target_id)
+                .unwrap_or(&Permissions::ADMINISTRATOR);
+
+            match &payload {
+                Payload::MessageCreate(_)
+                | Payload::MessageUpdate(_)
+                | Payload::MessageDelete(_) => {
+                    if p.has(Permissions::VIEW_CHANNEL).is_err() {
+                        continue;
+                    }
+                }
+
+                Payload::ChannelDelete(Empty::Default { id }) => {
+                    self.subscriptions.unsubscribe(id.to_string()).await.ok();
+                }
+
+                Payload::ChannelUpdate(channel) => {
+                    let server = if let Some(server_id) = channel.server_id {
+                        Some(server_id.server(None).await.unwrap())
+                    } else {
+                        None
+                    };
+
+                    let p = Permissions::fetch_cached(user, server.as_ref(), channel.into())
+                        .await
+                        .unwrap();
+
+                    permissions.insert(channel.id, p);
+                }
+
+                Payload::ServerMemberUpdate(member) => {
+                    if member.id == user.id {
+                        let p = Permissions::fetch(user, member.server_id.into(), None)
+                            .await
+                            .unwrap();
+                        permissions.insert(member.server_id, p);
+                    }
+                }
+
+                Payload::ServerMemberLeave(Empty::ServerObject { id, .. }) => {
+                    if *id == user.id {
+                        self.subscriptions
+                            .unsubscribe(target_id.to_string())
+                            .await
+                            .ok();
+                    }
+                }
+
+                Payload::ServerDelete(_) => {
+                    self.subscriptions
+                        .unsubscribe(target_id.to_string())
+                        .await
+                        .ok();
+                }
+
+                Payload::ServerCreate(server) => {
+                    self.subscriptions
+                        .subscribe(server.id.to_string())
+                        .await
+                        .ok();
+                }
+
+                Payload::ChannelCreate(channel) => {
+                    self.subscriptions
+                        .subscribe(channel.id.to_string())
+                        .await
+                        .ok();
+
+                    if !channel.in_server() {
+                        permissions.insert(
+                            channel.id,
+                            Permissions::fetch_cached(user, None, channel.into())
+                                .await
+                                .unwrap(),
+                        );
+                    }
+                }
+                Payload::UserUpdate(u) => {
+                    // Newly friend, blocked, request
+                    if u.id != target_id && u.id != user.id {
+                        self.subscriptions.subscribe(u.id.to_string()).await.ok();
+                    }
+                }
+                _ => {}
+            }
+
+            if self.send(payload).await.is_err() {
+                break;
+            }
         }
+    }
 
-        let payload = payload.unwrap();
+    pub async fn handle_incoming(&self, mut receiver: SplitStream<WebSocket>) {
+        while let Some(Ok(msg)) = receiver.next().await {
+            match msg {
+                Message::Text(content) => {
+                    let payload = serde_json::from_str::<ClientPayload>(&content);
 
-        match &payload {
-            ClientPayload::Authenticate { .. } => authenticate::run(self, payload).await,
-            ClientPayload::Ping => ping::run(self, payload).await,
+                    if payload.is_err() {
+                        log::debug!("Socket sent an invalid body");
+                        break;
+                    }
+
+                    let payload = payload.unwrap();
+
+                    match &payload {
+                        ClientPayload::Authenticate { .. } => {
+                            authenticate::run(self, payload).await
+                        }
+                        ClientPayload::Ping => ping::run(self, payload).await,
+                    }
+
+                    if self.user.lock().await.is_none() {
+                        log::debug!("Socket did not authenticate with valid token");
+                        break;
+                    }
+
+                    log::debug!("Socket Message: {:?}", content);
+                }
+                Message::Close(_) => break,
+                _ => {}
+            }
         }
-
-        log::debug!("Socket Message: {:?}", content);
     }
 }
--- src/gateway/upgrade.rs
+++ src/gateway/upgrade.rs
@@ -1,12 +1,8 @@
-use super::{client::Client, Payload};
+use super::client::Client;
 use crate::database::redis::*;
-use crate::{
-    gateway::Empty,
-    utils::{Permissions, Ref},
-};
 use axum::{
     extract::{
-        ws::{Message, WebSocket, WebSocketUpgrade},
+        ws::{WebSocket, WebSocketUpgrade},
         TypedHeader,
     },
     headers,
@@ -14,7 +10,6 @@ use axum::{
 };
 use futures::StreamExt;
 use std::sync::Arc;
-use tokio::sync::Mutex;
 
 pub async fn upgrade(
     ws: WebSocketUpgrade,
@@ -24,139 +19,19 @@ pub async fn upgrade(
 }
 
 async fn handle(ws: WebSocket) {
-    let (sender, mut receiver) = ws.split();
-    let sender = Arc::new(Mutex::new(sender));
-
+    let (sender, receiver) = ws.split();
     let subscriber = pubsub().await;
 
     // Resubscribe on reconnect
     subscriber.manage_subscriptions();
 
-    let original_client = Arc::new(Client::from(sender, subscriber));
+    let client_ref = Arc::new(Client::new(sender, subscriber));
 
-    let client = original_client.clone();
-    let mut receiver_task = tokio::spawn(async move {
-        while let Some(Ok(msg)) = receiver.next().await {
-            match msg {
-                Message::Text(content) => {
-                    client.on_message(content).await;
+    let client = client_ref.clone();
+    let mut receiver_task = tokio::spawn(async move { client.handle_incoming(receiver).await });
 
-                    if client.user.lock().await.is_none() {
-                        log::debug!("Socket did not authenticate with valid token");
-                        break;
-                    }
-                }
-                Message::Close(_) => break,
-                _ => {}
-            }
-        }
-    });
-
-    let client = original_client.clone();
-    let mut sender_task = tokio::spawn(async move {
-        while let Some((channel, payload)) = client.subscriptions.on_message().next().await {
-            let target_id: i64 = channel.parse().unwrap();
-            let user = client.user.lock().await;
-            let user = &*user.as_ref().unwrap();
-
-            let payload: Payload = serde_json::from_str(&payload.as_string().unwrap()).unwrap();
-            let mut permissions = client.permissions.lock().await;
-            let p = permissions
-                .get(&target_id)
-                .unwrap_or(&Permissions::ADMINISTRATOR);
-
-            match &payload {
-                Payload::MessageCreate(_)
-                | Payload::MessageUpdate(_)
-                | Payload::MessageDelete(_) => {
-                    if p.has(Permissions::VIEW_CHANNEL).is_err() {
-                        continue;
-                    }
-                }
-
-                Payload::ChannelDelete(Empty::Default { id }) => {
-                    client.subscriptions.unsubscribe(id.to_string()).await.ok();
-                }
-
-                Payload::ChannelUpdate(channel) => {
-                    let server = if let Some(server_id) = channel.server_id {
-                        Some(server_id.server(None).await.unwrap())
-                    } else {
-                        None
-                    };
-
-                    let p = Permissions::fetch_cached(user, server.as_ref(), channel.into())
-                        .await
-                        .unwrap();
-
-                    permissions.insert(channel.id, p);
-                }
-
-                Payload::ServerMemberUpdate(member) => {
-                    if member.id == user.id {
-                        let p = Permissions::fetch(user, member.server_id.into(), None)
-                            .await
-                            .unwrap();
-                        permissions.insert(member.server_id, p);
-                    }
-                }
-
-                Payload::ServerMemberLeave(Empty::ServerObject { id, .. }) => {
-                    if *id == user.id {
-                        client
-                            .subscriptions
-                            .unsubscribe(target_id.to_string())
-                            .await
-                            .ok();
-                    }
-                }
-
-                Payload::ServerDelete(_) => {
-                    client
-                        .subscriptions
-                        .unsubscribe(target_id.to_string())
-                        .await
-                        .ok();
-                }
-
-                Payload::ServerCreate(server) => {
-                    client
-                        .subscriptions
-                        .subscribe(server.id.to_string())
-                        .await
-                        .ok();
-                }
-
-                Payload::ChannelCreate(channel) => {
-                    client
-                        .subscriptions
-                        .subscribe(channel.id.to_string())
-                        .await
-                        .ok();
-
-                    if !channel.in_server() {
-                        permissions.insert(
-                            channel.id,
-                            Permissions::fetch_cached(user, None, channel.into())
-                                .await
-                                .unwrap(),
-                        );
-                    }
-                }
-                Payload::UserUpdate(u) => {
-                    // Newly friend, blocked, request
-                    if u.id != target_id && u.id != user.id {
-                        client.subscriptions.subscribe(u.id.to_string()).await.ok();
-                    }
-                }
-                _ => {}
-            }
-
-            if client.send(payload).await.is_err() {
-                break;
-            }
-        }
-    });
+    let client = client_ref.clone();
+    let mut sender_task = tokio::spawn(async move { client.handle_outgoing().await });
 
     tokio::select! {
         _ = (&mut sender_task) => receiver_task.abort(),
