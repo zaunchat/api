8a8a4f0a5fbb1873fb91ec98a079a64d1a11d78c
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-10-29 13:20:31 +0000

    fix: rmp_serde instead of rmp

--- src/gateway/client/config.rs
+++ src/gateway/client/config.rs
@@ -1,6 +1,6 @@
 use crate::gateway::{ClientPayload, Payload};
 use axum::extract::ws;
-use rmp as MsgPack;
+use rmp_serde as MsgPack;
 use serde::Deserialize;
 use serde_json as JSON;
 
@@ -35,13 +35,11 @@ impl SocketClientConfig {
     pub fn encode(&self, payload: Payload) -> ws::Message {
         match self.format {
             EncodingFormat::Json => {
-                ws::Message::Text(JSON::to_string(&payload).expect("Cannot stringify json"))
+                ws::Message::Text(JSON::to_string(&payload).expect("Cannot serialise data (json)"))
             }
-            EncodingFormat::MsgPack => {
-                let mut buf = Vec::new();
-                MsgPack::encode::write_str(&mut buf, &JSON::to_string(&payload).unwrap()).unwrap();
-                ws::Message::Binary(buf)
-            }
+            EncodingFormat::MsgPack => ws::Message::Binary(
+                MsgPack::encode::to_vec_named(&payload).expect("Cannot serialise data (msgpack)"),
+            ),
         }
     }
 
@@ -52,13 +50,7 @@ impl SocketClientConfig {
                 _ => None,
             },
             ws::Message::Binary(buf) => match self.format {
-                EncodingFormat::MsgPack => {
-                    let mut content = Vec::new();
-
-                    MsgPack::decode::read_str(&mut &buf[..], &mut content).ok();
-
-                    JSON::from_slice(&content).ok()
-                }
+                EncodingFormat::MsgPack => MsgPack::from_slice(&buf).ok(),
                 _ => None,
             },
             _ => None,
